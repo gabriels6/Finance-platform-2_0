d14ec27cd0d25c369e29f1a8b61072c3b8388807
Gabriel Gomes <gabrielsogom@gmail.com>
2024-07-14 16:55:42 -0300

    V1.8.31 Fixing nullcheck

--- src/pages/AssetMaintaince/index.jsx
+++ src/pages/AssetMaintaince/index.jsx
@@ -152,7 +152,7 @@ const AssetMaintaince = () => {
             currency: selectedAsset.currency?.symbol,
             asset_type: selectedAsset.asset_type?.name,
             external_id: selectedAsset.external_id,
-            country: selectedAsset.country.name,
+            country: selectedAsset.country?.name,
             yahoo_code: selectedAsset.yahoo_code,
             sector: selectedAsset.sector?.name,
             investing_external_id: selectedAsset.investing_external_id
