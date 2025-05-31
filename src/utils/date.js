e09b9638eae692765c5bba905c6bf85161dc8f21
Gabriel Gomes <gabrielsogom@gmail.com>
2025-05-31 18:54:03 -0300

    1.3.2 Adjusting forwards

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,5 +1,5 @@
 export default {
     formatDate(date = new Date()) {
-        return date.toLocaleDateString().split("/").reverse().join("-")
+        return date?.toLocaleDateString()?.split("/")?.reverse()?.join("-")
     }
 }
